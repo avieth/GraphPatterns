934ecc38a0b6f380dea9fd94c4c2ea30c821a5da
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2014-10-14 18:23:33 -0400

    Express cardinality constraints in types

--- Data/GraphPatterns/GraphEngine.hs
+++ Data/GraphPatterns/GraphEngine.hs
@@ -1,10 +1,74 @@
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Data.GraphPatterns.GraphEngine (
     GraphEngine(..)
+  , ManyToOne
+  , OneToMany
+  , ManyToMany
+  , OneToOne
+  , EdgeLabel(..)
+  , Incoming
+  , Outgoing
+  , Both
+  , EdgeTraversalResult
+  , Many
+  , One
   ) where
 
 import Control.Applicative (Applicative)
+import Data.Functor.Identity (Identity, runIdentity)
+import Data.Traversable (Traversable)
+import Data.Foldable (Foldable)
+
+data ManyToOne
+data OneToMany
+data ManyToMany
+data OneToOne
+
+class EdgeLabel l where
+  -- | Must be one of ManyToOne, OneToMany, ManyToMany, OneToOne
+  --   Can we enforce this?
+  type EdgeCardinality l :: *
+  toEngine :: GraphEngine m => l -> EngineEdgeLabel m
+
+data Incoming
+data Outgoing
+data Both
+
+-- | Type to represent many of some thing. Naturally, we choose the list.
+newtype Many a = Many [a]
+  deriving (Functor, Applicative, Monad, Foldable, Traversable)
+
+-- | Type to represent at most one of some thing. Naturally, we choose maybe.
+newtype One a = One (Maybe a)
+  deriving (Functor, Applicative, Monad, Foldable, Traversable)
+
+-- Image of this type family is the type to use for
+--   getEdgesOut if direction ~ Outgoing
+--   getEdgesIn if direction ~ Incoming
+-- and the edge label l has EdgeCardinality l ~ cardinality.
+-- TODO should be closed type family but I don't have GHC 7.8.1
+--
+-- Need to tell GHC that every outcome of this family is a Traversable. How to
+-- do that?
+--
+--   class EdgeTraversalResult direction cardinality a where
+--     type 
+--     
+type family EdgeTraversalResult direction cardinality a
+type instance EdgeTraversalResult Incoming ManyToOne a = Many a
+type instance EdgeTraversalResult Incoming ManyToMany a = Many a
+type instance EdgeTraversalResult Incoming OneToMany a = One a
+type instance EdgeTraversalResult Incoming OneToOne a = One a
+type instance EdgeTraversalResult Outgoing ManyToOne a = One a
+type instance EdgeTraversalResult Outgoing ManyToMany a = Many a
+type instance EdgeTraversalResult Outgoing OneToMany a = Many a
+type instance EdgeTraversalResult Outgoing OneToOne a = One a
+type instance EdgeTraversalResult Both ManyToOne a = Many a
+type instance EdgeTraversalResult Both ManyToMany a = Many a
+type instance EdgeTraversalResult Both OneToMany a = Many a
+type instance EdgeTraversalResult Both OneToOne a = One a
 
 -- | Class to describe anything that can be used as a graph engine.
 --   We don't want to force it to be in IO, because pure Haskell graph
@@ -12,9 +76,6 @@ import Control.Applicative (Applicative)
 --   because without these, it simply wouldn't work. For pure graph engines
 --   like graph-core, all you've got to do is wrap them in an appropriate
 --   datatype.
---   If your graph is undirected, you need only implement one of
---   getEdgesOut/getEdgesIn. If you implement both, make sure that
---     getEdgesOut v = getEdgesIn v
 class (Functor m, Applicative m, Monad m) => GraphEngine m where
 
   -- | Type of the thing from which we pull our info. Needed only for
@@ -31,7 +92,7 @@ class (Functor m, Applicative m, Monad m) => GraphEngine m where
 
   -- | Type of edge labels. If a given implementation of Graph does not support
   --   this notion, you should use the type ().
-  type EdgeLabel m :: *
+  type EngineEdgeLabel m :: *
 
   -- | Type of vertices.
   type Vertex m :: *
@@ -51,15 +112,23 @@ class (Functor m, Applicative m, Monad m) => GraphEngine m where
 
   -- | Given a Vertex, produce a list of all Edges outgoing, i.e. with tail
   --   ends at the Vertex.
-  --   If your Graph is undirected, this is the same as getEdgesIn
-  getEdgesOut :: EdgeLabel m -> Vertex m -> m [Edge m]
-  getEdgesOut = getEdgesIn
+  --   Cannot give a default implementation for undirected graphs because we
+  --   have the Outgoing type in there.
+  getEdgesOut
+    :: (EdgeLabel l)
+    => l
+    -- ^ The intention is that this guy is a value used only for its type a la
+    -- singleton types... call it a singleton value?
+    -> Vertex m
+    -> m (EdgeTraversalResult Outgoing (EdgeCardinality l) (Edge m))
 
   -- | Given a Vertex, produce a list of all Edges incoming, i.e. with tail
   --   ends at the Vertex.
-  --   If your Graph is undirected, this is the same as getEdgesOut
-  getEdgesIn :: EdgeLabel m -> Vertex m -> m [Edge m]
-  getEdgesIn = getEdgesOut
+  getEdgesIn
+    :: (EdgeLabel l)
+    => l
+    -> Vertex m
+    -> m (EdgeTraversalResult Incoming (EdgeCardinality l) (Edge m))
 
   -- | Get the vertex to which an edge goes in (head of edge is here).
   getTargetVertex :: Edge m -> m (Vertex m)
--- Data/GraphPatterns/Language.hs
+++ Data/GraphPatterns/Language.hs
@@ -11,12 +11,14 @@ module Data.GraphPatterns.Language (
   , adjacentIn
   , adjacent
   , hopIncoming
-  , hopOutgoing
+  --, hopOutgoing
   ) where
 
 import Data.GraphPatterns.GraphEngine
 
-import Control.Applicative ((<$>), (<*>))
+import Control.Applicative ((<$>), (<*>), Applicative)
+import Control.Monad (join)
+import Data.Traversable (traverse, Traversable)
 
 -- Some declarative sugar for GraphEngine methods (no verbs).
 
@@ -26,10 +28,18 @@ vertex = getVertexById
 edge :: GraphEngine m => EdgeId m -> m (Maybe (Edge m))
 edge = getEdgeById
 
-incoming :: GraphEngine m => EdgeLabel m -> Vertex m -> m [Edge m]
+incoming
+  :: (GraphEngine m, EdgeLabel l)
+  => l
+  -> Vertex m
+  -> m (EdgeTraversalResult Incoming (EdgeCardinality l) (Edge m))
 incoming = getEdgesIn
 
-outgoing :: GraphEngine m => EdgeLabel m -> Vertex m -> m [Edge m]
+outgoing
+  :: (GraphEngine m, EdgeLabel l)
+  => l
+  -> Vertex m
+  -> m (EdgeTraversalResult Outgoing (EdgeCardinality l) (Edge m))
 outgoing = getEdgesOut
 
 source :: GraphEngine m => Edge m -> m (Vertex m)
@@ -38,36 +48,99 @@ source = getSourceVertex
 target :: GraphEngine m => Edge m -> m (Vertex m)
 target = getTargetVertex
 
-adjacentOut :: GraphEngine m => EdgeLabel m -> Vertex m -> m [Vertex m]
+-- WART: I want this type signature...
+--adjacentOut
+--  :: (GraphEngine m, EdgeLabel l)
+--  => l
+--  -> Vertex m
+--  -> m (EdgeTraversalResult Outgoing (EdgeCardinality l) (Vertex m))
+-- ... but I have to give
+adjacentOut
+  :: (Traversable t, GraphEngine m, EdgeLabel l, EdgeTraversalResult Outgoing (EdgeCardinality l) (Edge m) ~ t (Edge m))
+  => l
+  -> Vertex m
+  -> m (t (Vertex m))
+-- because I haven't told GHC that every image of EdgeTraversalResult is a
+-- Traversable. Is that possible?
 adjacentOut el v = do
   es <- getEdgesOut el v
-  mapM getTargetVertex es
+  -- Shit, we don't know what the type of es is!
+  -- We've got to dispatch this at the type level, as part of a class perhaps?
+  -- Maybe we could have some function
+  --
+  -- class Magic c where
+  --   magic :: ([a] -> b) -> EdgeTraversalResult _ c _ -> b
+  -- 
+  -- instance Magic Incoming ManyToMany where
+  --   magic f x = mapM
+  --
+  -- We know that es is a monad (either Identity or []) so we can map into
+  -- it
+  traverse getTargetVertex es
 
-adjacentIn :: GraphEngine m => EdgeLabel m -> Vertex m -> m [Vertex m]
-adjacentIn el v = do
-  es <- getEdgesIn el v
-  mapM getSourceVertex es
+adjacentIn
+  :: (Traversable t, GraphEngine m, EdgeLabel l, EdgeTraversalResult Incoming (EdgeCardinality l) (Edge m) ~ t (Edge m))
+  => l
+  -> Vertex m
+  -> m (t (Vertex m))
+adjacentIn el v = getEdgesIn el v >>= traverse getSourceVertex
 
-adjacent :: GraphEngine m => EdgeLabel m -> Vertex m -> m [Vertex m]
+adjacent
+  :: (Traversable t, GraphEngine m, EdgeLabel l, EdgeTraversalResult Both (EdgeCardinality l) (Edge m) ~ t (Edge m))
+  => l
+  -> Vertex m
+  -> m (t (Vertex m))
 adjacent el v = do
   esIn <- getEdgesIn el v
   esOut <- getEdgesOut el v
-  (++) <$> mapM getSourceVertex esIn <*> mapM getTargetVertex esOut
+  -- Oh no, what are the types!?!?!
+  --(++) <$> mapM getSourceVertex esIn <*> mapM getTargetVertex esOut
+  -- Perhaps we need a class
+  --
+  --   class Magic a b c where
+  --     combine a b :: a -> b -> c
+  --
+  --   instance Magic (One a) (Many a) (Many a) where
+  --     combine (One x) (Many ys) = Many $ (runIdentity x) : ys
+  --
+  --   instance Magic (Many a) (One a) (Many a) where
+  --     combine (Many xs) (One y) = Many $ xs ++ [runIdentity y]
+  --
+  -- ...
+  return undefined
+
+-- | A traversal plus a join on the inner monad (the traversable is also a
+--   monad).
+flatTraverse
+  :: (Traversable t, Monad t, Applicative f)
+  => (a -> f (t b))
+  -> t a
+  -> f (t b)
+flatTraverse = (doublefmap join) . traverse
+  where doublefmap = fmap . fmap
 
 -- Hop n times on incoming edges.
-hopIncoming :: GraphEngine m => Int -> EdgeLabel m -> Vertex m -> m ([Vertex m])
+hopIncoming
+  :: ( Monad t, Traversable t, GraphEngine m, EdgeLabel l
+     , EdgeTraversalResult Incoming (EdgeCardinality l) (Edge m) ~ t (Edge m))
+  => Int
+  -> l
+  -> Vertex m
+  -> m (t (Vertex m))
 hopIncoming n l v
-  | n > 0 = do
-    ws <- adjacentIn l v
-    concat <$> mapM (hopIncoming (n-1) l) ws
+  | n > 0 = adjacentIn l v >>= flatTraverse (hopIncoming (n-1) l)
   -- We return v even if n < 0. Better than error? Not sure.
-  | otherwise = return [v]
+  | otherwise = return . return $ v
 
 -- Hop n times on outgoing edges.
-hopOutgoing :: GraphEngine m => Int -> EdgeLabel m -> Vertex m -> m ([Vertex m])
+hopOutgoing
+  :: ( Monad t, Traversable t, GraphEngine m, EdgeLabel l
+     , EdgeTraversalResult Outgoing (EdgeCardinality l) (Edge m) ~ t (Edge m))
+  => Int
+  -> l
+  -> Vertex m
+  -> m (t (Vertex m))
 hopOutgoing n l v
-  | n > 0 = do
-    ws <- adjacentOut l v
-    concat <$> mapM (hopIncoming (n-1) l) ws
+  | n > 0 = adjacentOut l v >>= flatTraverse (hopOutgoing (n-1) l)
   -- We return v even if n < 0. Better than error? Not sure.
-  | otherwise = return [v]
+  | otherwise = return . return $ v
