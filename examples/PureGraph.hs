0612cd159d0f5db690e7661de366154bb253d782
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2014-10-13 20:05:11 -0400

    Example: GraphEngine instance for Data.Graph
    from containers

---
+++ examples/PureGraph.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+module PureGraph (
+    PureGraph
+  ) where
+
+import Data.Graph as G
+import Data.Void (Void)
+import Control.Applicative (Applicative)
+
+import Data.GraphPatterns.GraphEngine
+
+newtype PureGraph a = PureGraph (G.Graph -> a)
+  deriving (Functor, Applicative, Monad)
+
+-- | A description of how the containers Data.Graph type can be used as a
+--   GraphEngine.
+instance GraphEngine PureGraph where
+  type Graph PureGraph = G.Graph
+  type Vertex PureGraph = G.Vertex
+  type Edge PureGraph = G.Edge
+  type EdgeId PureGraph = Void
+  type VertexId PureGraph = G.Vertex
+  type EdgeLabel PureGraph = ()
+  runGraphEngine (PureGraph f) g = f g
+  getVertexById vid = PureGraph $ \g -> case (elem vid) $ vertices g of
+    True -> Just vid
+    False -> Nothing
+  -- | This one can never be called, because EdgeId PureGraph = Void!
+  getEdgeById = undefined
+  getEdgesOut _ v = PureGraph $ \g -> filter predicate $ edges g
+    where predicate (x, _) = x == v
+  getEdgesIn _ v = PureGraph $ \g -> filter predicate $ edges g
+    where predicate (_, x) = x == v
+  getTargetVertex = return . snd
+  getSourceVertex = return . fst
