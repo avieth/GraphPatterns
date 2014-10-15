e8a36326e5f7a2cc868d823dcf84b1c97ca3bee7
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2014-10-14 23:57:48 -0400

    PureGraph example updated for data families
    Not so trivial...

--- examples/PureGraph.hs
+++ examples/PureGraph.hs
@@ -18,22 +18,24 @@ newtype PureGraph a = PureGraph (G.Graph -> a)
 --   GraphEngine.
 instance GraphEngine PureGraph where
   type Graph PureGraph = G.Graph
-  type Vertex PureGraph = G.Vertex
-  type Edge PureGraph = G.Edge
+  data Vertex PureGraph = PureGraphVertex G.Vertex
+  data Edge PureGraph = PureGraphEdge G.Edge
   type EdgeId PureGraph = Void
   type VertexId PureGraph = G.Vertex
   data EngineEdgeLabel PureGraph = PureGraphEdgeLabel ()
   runGraphEngine (PureGraph f) g = f g
   getVertexById vid = PureGraph $ \g -> case (elem vid) $ vertices g of
-    True -> Just vid
+    True -> Just $ PureGraphVertex vid
     False -> Nothing
   -- | This one can never be called, because EdgeId PureGraph = Void!
   getEdgeById = undefined
   getEdgesOut l v = PureGraph $ \g ->
-      handleAnomaly Outgoing l (filter predicate $ edges g)
-    where predicate (x, _) = x == v
+      handleAnomaly Outgoing l v (map PureGraphEdge (filter predicate $ edges g))
+    where predicate (x, _) = x ^== v
   getEdgesIn l v = PureGraph $ \g ->
-      handleAnomaly Incoming l (filter predicate $ edges g)
-    where predicate (_, x) = x == v
-  getTargetVertex = return . snd
-  getSourceVertex = return . fst
+      handleAnomaly Incoming l v (map PureGraphEdge (filter predicate $ edges g))
+    where predicate (_, x) = x ^== v
+  getTargetVertex (PureGraphEdge (_, i)) = return $ PureGraphVertex i
+  getSourceVertex (PureGraphEdge (i, _)) = return $ PureGraphVertex i
+
+(^==) x (PureGraphVertex i) = x == i
