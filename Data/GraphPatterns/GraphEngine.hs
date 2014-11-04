c68fb752ba031822c919e91629b6ab5f552810b8
Alexander Vieth <alexander.vieth@mail.mcgill.ca>
2014-11-04 18:44:44 -0500

    GraphEngine has types for insertion data
    The motivation: we can't ask the user to create EngineVertex/Edge
    instances; we want the GraphEngine to be the only one capable of doing
    this. Case in point: if an engine wants to generate its own identifiers,
    how is the user to produce a valid EngineVertex/Edge for insertion?
    
    So, the idea is to create an Engine(Vertex|Edge)Insertion datatype which
    can be used, via insertVertex(Edge) to get an EngineVertex(Edge).

--- Data/GraphPatterns/GraphEngine.hs
+++ Data/GraphPatterns/GraphEngine.hs
@@ -32,29 +32,42 @@ class (Functor m, Applicative m, Monad m) => GraphEngine m where
   --   runGraphEngine.
   type EngineGraph m :: *
 
-  -- | Type of vertices.
-  data EngineVertex m :: *
+  -- | Type of vertices. Each member of this type must identify at most one
+  --   actual vertex in an EngineGraph.
+  data EngineVertex m v :: *
 
-  -- | Type of edges.
-  data EngineEdge m :: *
+  -- | Type of edges. Each member of this type must identify at most one
+  --   actual edge in an EngineGraph.
+  data EngineEdge m e :: *
 
-  -- | Type of (possibly partial) vertex information.
-  data EngineVertexInformation m :: *
+  -- | Vertex and Edge instances must provide injections into these types so
+  --   that edges and vertices can be inserted into a graph. The GraphEngine
+  --   will produce EngineVertex and EngineEdge instances from these via
+  --   insertVertex, insertEdge.
+  data EngineVertexInsertion m v :: *
+  data EngineEdgeInsertion m e :: *
 
-  -- | Type of (possibly partial) edge information.
-  data EngineEdgeInformation m :: *
+  -- | Type of (possibly partial) vertex information. Each member of this type
+  --   must characterize 0 or more vertices in an EngineGraph.
+  data EngineVertexInformation m v :: *
+
+  -- | Type of (possibly partial) edge information. Each member of this type
+  --   must characterize 0 or more edges in an EngineGraph.
+  data EngineEdgeInformation m e :: *
 
   -- | Get the vertex to which an edge goes in (head of edge is here).
-  getTargetVertex :: EngineEdge m -> m (EngineVertex m)
+  --   Must wrap in Maybe because the input edge could be bogus.
+  getTargetVertex :: EngineEdge m e -> m (Maybe (EngineVertex m v))
 
   -- | Get the vertex from which an edge goes out (tail of edge is here).
-  getSourceVertex :: EngineEdge m -> m (EngineVertex m)
+  --   Must wrap in Maybe because the input edge could be bogus.
+  getSourceVertex :: EngineEdge m e -> m (Maybe (EngineVertex m v))
 
   -- | Get zero, one, or many EngineVertex from relevant information.
-  getVertices :: EngineVertexInformation m -> m [EngineVertex m]
+  getVertices :: EngineVertexInformation m v -> m [EngineVertex m v]
 
   -- | Get zero, one, or many EngineEdge from relevant information.
-  getEdges :: EngineEdgeInformation m -> m [EngineEdge m]
+  getEdges :: EngineEdgeInformation m e -> m [EngineEdge m e]
 
   -- | Given a Vertex, produce a list of all Edges outgoing, i.e. with tail
   --   ends at the Vertex.
@@ -80,18 +93,18 @@ class (Functor m, Applicative m, Monad m) => GraphEngine m where
   -- Instead, we choose to keep EngineEdgeInformation divorced from topology.
   -- An EngineEdgeInformation can make no statement about where that edge lies
   -- in a graph.
-  getEdgesOut :: EngineEdgeInformation m -> EngineVertex m -> m [EngineEdge m]
+  getEdgesOut :: EngineEdgeInformation m e -> EngineVertex m v -> m [EngineEdge m e]
 
   -- | Given a Vertex, produce a list of all Edges incoming, i.e. with tail
   --   ends at the Vertex.
-  getEdgesIn :: EngineEdgeInformation m -> EngineVertex m -> m [EngineEdge m]
+  getEdgesIn :: EngineEdgeInformation m e -> EngineVertex m v -> m [EngineEdge m e]
 
-  insertVertex :: EngineVertex m -> m Bool
+  insertVertex :: EngineVertexInsertion m v -> m (Maybe (EngineVertex m v))
   -- ^ Must give True and only if the vertex was successfully inserted.
 
   insertEdge
-    :: EngineEdge m
-    -> EngineVertex m
-    -> EngineVertex m
-    -> m Bool
+    :: EngineEdgeInsertion m e
+    -> EngineVertex m u
+    -> EngineVertex m v
+    -> m (Maybe (EngineEdge m e))
   -- ^ Regarding the Bool: same contract as for insertVertex
